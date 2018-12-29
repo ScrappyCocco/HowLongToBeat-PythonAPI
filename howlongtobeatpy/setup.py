1775a77e90bfc4d7d61b0a926d74eaac364dd161
Michele <mikelerogeratro@yahoo.it>
2018-12-29 13:10:12 +0100

    Added long description and gitignore

--- howlongtobeatpy/setup.py
+++ howlongtobeatpy/setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='howlongtobeatpy',
-      version='0.1',
+      version='0.1.1',
       packages=find_packages(exclude=['tests']),
       description='A Python API for How Long to Beat',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/ScrappyCocco/HowLongToBeat-PythonAPI',
       author='ScrappyCocco',
       license='MIT',
       keywords='howlongtobeat gaming steam uplay origin time length how long to beat',
       install_requires=[
-            'aiohttp',
-            'requests',
-            'aiounittest'
+          'aiohttp',
+          'requests',
+          'aiounittest'
       ],
       zip_safe=False)
