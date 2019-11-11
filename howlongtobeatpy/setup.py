4a8d1411c05384a38ad9df5417baa75e4ac0ccec
Michele <mikelerogeratro@yahoo.it>
2019-11-11 13:02:31 +0100

    Fix setup requirements and version update

--- howlongtobeatpy/setup.py
+++ howlongtobeatpy/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='howlongtobeatpy',
-      version='0.1.6',
+      version='0.1.7',
       packages=find_packages(exclude=['tests']),
       description='A Python API for How Long to Beat',
       long_description=long_description,
@@ -16,8 +16,6 @@ setup(name='howlongtobeatpy',
       install_requires=[
           'aiohttp',
           'requests',
-          'aiounittest',
-          're',
-          'html'
+          'aiounittest'
       ],
       zip_safe=False)
