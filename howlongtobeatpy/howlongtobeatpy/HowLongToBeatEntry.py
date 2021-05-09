b0b3cc9ad95f6006ff8140454230bbdc60424a0e
PydraxAlpta <49099987+PydraxAlpta@users.noreply.github.com>
2021-05-09 19:50:12 +0530

    changed from repr to str
    Changed to the str method and removed print statements, and returned the formed string

--- howlongtobeatpy/howlongtobeatpy/HowLongToBeatEntry.py
+++ howlongtobeatpy/howlongtobeatpy/HowLongToBeatEntry.py
@@ -1,10 +1,10 @@
 class HowLongToBeatEntry:
     """
     A simple class to collect all game data that are being read from the HTML code
-    The first value of each section contain the actual time (or -1/0 if not available)
-    The second value of each section contain the unit of the time (Minutes/Hours, or None if not available)
+    The first value of each section contain the actual time (or -1/0 if not available
+    The second value of each section contain the unit of the time (Minutes/Hours, or None if not available
     The third value of each section contains the label
-        (Can be Main Story/Main + Extras/Completionist/Single-Player/Solo/Co-Op/Vs.)
+        (Can be Main Story/Main + Extras/Completionist/Single-Player/Solo/Co-Op/Vs.
     """
 
     def __init__(self):
@@ -28,23 +28,8 @@ class HowLongToBeatEntry:
         self.gameplay_completionist_label = None
         # Similarity with original name
         self.similarity = -1
-    def __repr__(self):
-        print("game_id:",self.game_id )
-        print("game_name:",self.game_name ) 
-        print("game_name_suffix:",self.game_name_suffix ) 
-        print("game_image_url:",self.game_image_url ) 
-        print("game_web_link:",self.game_web_link ) 
-        # Gameplay Main
-        print("gameplay_main:",self.gameplay_main ) 
-        print("gameplay_main_unit:",self.gameplay_main_unit ) 
-        print("gameplay_main_label:",self.gameplay_main_label ) 
-        # Gameplay Main + Extra
-        print("gameplay_main_extra:",self.gameplay_main_extra ) 
-        print("gameplay_main_extra_unit:",self.gameplay_main_extra_unit ) 
-        print("gameplay_main_extra_label:",self.gameplay_main_extra_label ) 
-        # Completionist
-        print("gameplay_completionist:",self.gameplay_completionist ) 
-        print("gameplay_completionist_unit:",self.gameplay_completionist_unit ) 
-        print("gameplay_completionist_label:",self.gameplay_completionist_label ) 
-        # Similarity with original name
-        print("similarity:",self.similarity)
+
+    def __str__(self):
+        tup = ("game_id:", self.game_id,"game_name:", self.game_name,"game_name_suffix:", self.game_name_suffix,"game_image_url:", self.game_image_url,"game_web_link:", self.game_web_link,"gameplay_main:", self.gameplay_main,"gameplay_main_unit:", self.gameplay_main_unit,"gameplay_main_label:", self.gameplay_main_label,"gameplay_main_extra:",self.gameplay_main_extra,"gameplay_main_extra_unit:", self.gameplay_main_extra_unit,"gameplay_main_extra_label:", self.gameplay_main_extra_label,"gameplay_completionist:", self.gameplay_completionist,"gameplay_completionist_unit:", self.gameplay_completionist_unit,"gameplay_completionist_label:", self.gameplay_completionist_label,"similarity:", self.similarity)
+        string = " ".join([str(x) for x in tup])
+        return string
