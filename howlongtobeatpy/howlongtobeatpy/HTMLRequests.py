4b9c24073a6f0bd4f46e8292f26c50faceb61c0b
Michele <mikelerogeratro@yahoo.it>
2022-05-22 22:23:28 +0200

    Reduced code duplication by a lot to make sonarcloud happy

--- howlongtobeatpy/howlongtobeatpy/HTMLRequests.py
+++ howlongtobeatpy/howlongtobeatpy/HTMLRequests.py
@@ -26,21 +26,27 @@ class HTMLRequests:
     ORIGIN_HEADER = 'https://howlongtobeat.com'
     REFERER_HEADER = BASE_URL
     SEARCH_URL = BASE_URL + "search_results"
-    GAME_URL = BASE_URL + "game?id="
+    GAME_URL = BASE_URL + "game"
 
     @staticmethod
-    def send_web_request(game_name: str, search_modifiers: SearchModifiers = SearchModifiers.NONE, page: int = 1):
+    def get_search_request_parameters(page: int):
         """
-        Function that search the game using a normal request
-        @param game_name: The original game name received as input
-        @param search_modifiers: The "Modifiers" list in "Search Options", allow to show/isolate/hide DLCs
-        @param page: The page to explore of the research, unknown if this is actually used
-        @return: The HTML code of the research if the request returned 200(OK), None otherwise
+        Generate the parameters for the search request
+        @param page: The page to search
+        @return: The parameters object for the request
         """
-        ua = UserAgent()
         params = {
             'page': str(page)
         }
+        return params
+
+    @staticmethod
+    def get_search_request_headers():
+        """
+        Generate the headers for the search request
+        @return: The headers object for the request
+        """
+        ua = UserAgent()
         headers = {
             'content-type': 'application/x-www-form-urlencoded',
             'accept': '*/*',
@@ -48,6 +54,16 @@ class HTMLRequests:
             'origin': HTMLRequests.ORIGIN_HEADER,
             'referer': HTMLRequests.REFERER_HEADER
         }
+        return headers
+
+    @staticmethod
+    def get_search_request_data(game_name: str, search_modifiers: SearchModifiers):
+        """
+        Generate the data payload for the search request
+        @param game_name: The name of the game to search
+        @param search_modifiers: The search modifiers to use in the search
+        @return: The request (data) payload object for the request
+        """
         payload = {
             'queryString': game_name,
             't': 'games',
@@ -63,6 +79,20 @@ class HTMLRequests:
             'g': '',
             'randomize': '0'
         }
+        return payload
+
+    @staticmethod
+    def send_web_request(game_name: str, search_modifiers: SearchModifiers = SearchModifiers.NONE, page: int = 1):
+        """
+        Function that search the game using a normal request
+        @param game_name: The original game name received as input
+        @param search_modifiers: The "Modifiers" list in "Search Options", allow to show/isolate/hide DLCs
+        @param page: The page to explore of the research, unknown if this is actually used
+        @return: The HTML code of the research if the request returned 200(OK), None otherwise
+        """
+        params = HTMLRequests.get_search_request_parameters(page)
+        headers = HTMLRequests.get_search_request_headers()
+        payload = HTMLRequests.get_search_request_data(game_name, search_modifiers)
         # Make the post request and return the result if is valid
         resp = requests.post(HTMLRequests.SEARCH_URL, params=params, headers=headers, data=payload)
         if resp.status_code == 200:
@@ -79,32 +109,9 @@ class HTMLRequests:
         @param page: The page to explore of the research, unknown if this is actually used
         @return: The HTML code of the research if the request returned 200(OK), None otherwise
         """
-        ua = UserAgent()
-        params = {
-            'page': str(page)
-        }
-        headers = {
-            'content-type': 'application/x-www-form-urlencoded',
-            'accept': '*/*',
-            'User-Agent': ua.random,
-            'origin': HTMLRequests.ORIGIN_HEADER,
-            'referer': HTMLRequests.REFERER_HEADER
-        }
-        payload = {
-            'queryString': game_name,
-            't': 'games',
-            'sorthead': 'popular',
-            'sortd': 'Normal Order',
-            'plat': '',
-            'length_type': 'main',
-            'length_min': '',
-            'length_max': '',
-            'detail': search_modifiers.value,
-            'v': '',
-            'f': '',
-            'g': '',
-            'randomize': '0'
-        }
+        params = HTMLRequests.get_search_request_parameters(page)
+        headers = HTMLRequests.get_search_request_headers()
+        payload = HTMLRequests.get_search_request_data(game_name, search_modifiers)
         # Make the post request and return the result if is valid
         async with aiohttp.ClientSession() as session:
             async with session.post(HTMLRequests.SEARCH_URL, params=params, headers=headers, data=payload) as resp:
@@ -132,6 +139,31 @@ class HTMLRequests:
         return cut_title
 
     @staticmethod
+    def get_title_request_parameters(game_id: int):
+        """
+        enerate the parameters for the search request
+        @param game_id: The game id to search in HLTB
+        @return: The parameters object for the request
+        """
+        params = {
+            'id': str(game_id)
+        }
+        return params
+
+    @staticmethod
+    def get_title_request_headers():
+        """
+        Generate the headers for the search request
+        @return: The headers object for the request
+        """
+        ua = UserAgent()
+        headers = {
+            'User-Agent': ua.random,
+            'referer': HTMLRequests.REFERER_HEADER
+        }
+        return headers
+
+    @staticmethod
     def get_game_title(game_id: int):
         """
         Function that gets the title of a game from the game (howlongtobeat) id
@@ -139,14 +171,8 @@ class HTMLRequests:
         @return: The game title from the given id
         """
 
-        ua = UserAgent()
-        params = {
-            'id': str(game_id)
-        }
-        headers = {
-            'User-Agent': ua.random,
-            'referer': HTMLRequests.REFERER_HEADER
-        }
+        params = HTMLRequests.get_title_request_parameters(game_id)
+        headers = HTMLRequests.get_title_request_headers()
 
         # Request and extract title
         contents = requests.get(HTMLRequests.GAME_URL, params=params, headers=headers)
@@ -160,14 +186,8 @@ class HTMLRequests:
         @return: The game title from the given id
         """
 
-        ua = UserAgent()
-        params = {
-            'id': str(game_id)
-        }
-        headers = {
-            'User-Agent': ua.random,
-            'referer': HTMLRequests.REFERER_HEADER
-        }
+        params = HTMLRequests.get_title_request_parameters(game_id)
+        headers = HTMLRequests.get_title_request_headers()
 
         # Request and extract title
         async with aiohttp.ClientSession() as session:
